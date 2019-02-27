345b8ca2f489f5abb5b3e385cada05fd9ac59880
nthomasCUBE <thomas.nussbaumer@univie.ac.at>
2019-02-27 17:42:48 +0100

    Update 0_convert_example_data.R

--- demo_data/0_convert_example_data.R
+++ demo_data/0_convert_example_data.R
@@ -1,3 +1,7 @@
 d1=read.csv("data/FemaleLiver-Data/ClinicalTraits.csv",sep=",",header=T,row.names=1)
 write.table(d1,"Traits_modified.txt",sep="\t",row.names=FALSE,quote=FALSE)
 
+d1=read.csv("data/FemaleLiver-Data/LiverFemale3600.csv",sep=",",header=T)
+d1=cbind("Gene"=rn,d1[,9:dim(d1)[2]])
+write.table(d1,"Expression_modified.txt",sep="\t",row.names=FALSE,quote=FALSE)
+
