01c525301416503bf7b3ec945394f8a758f37189
nthomasCUBE <thomas.nussbaumer@univie.ac.at>
2019-02-27 17:21:49 +0100

    Update 0_convert_example_data.R

--- demo_data/0_convert_example_data.R
+++ demo_data/0_convert_example_data.R
@@ -1,4 +1,3 @@
 d1=read.csv("data/FemaleLiver-Data/ClinicalTraits.csv",sep=",",header=T,row.names=1)
-write.table(d1,"Traits_modified.txt",sep="\t",row.names=FALSE)
-
+write.table(d1,"Traits_modified.txt",sep="\t",row.names=FALSE,quote=FALSE)
 
