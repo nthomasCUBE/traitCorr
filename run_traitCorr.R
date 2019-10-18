c661d32392ae8c1e99a73057f02bed706b5faec0
nthomasCUBE <thomas.nussbaumer@univie.ac.at>
2019-10-18 17:09:55 +0200

    Update run_traitCorr.R

--- run_traitCorr.R
+++ run_traitCorr.R
@@ -1,6 +1,7 @@
 packages_necessary=c("d3heatmap","gplots","officer","VennDiagram","scales","shiny","shinyalert","shinyBS","shinyjs","shinythemes","xlsx")
 for(x in 1:length(packages_necessary)){
-  library(packages_necessary[x],character.only=FALSE)
+  print(packages_necessary[x])
+  library(packages_necessary[x],character.only=TRUE)
 }
 
 source("methods.R")
